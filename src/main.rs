9ebc5de47292feeaa1051df1bf2a22beb3b0f8d8
DerFluep <36232559+DerFluep@users.noreply.github.com>
2026-01-05 20:06:59 +0100

    Optimize the crap out of it, ~75x speed boost
    unfortunately no more red pixel, a small price to pay

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,16 @@ use std::sync::atomic::AtomicBool;
 use crate::simulation::Simulation;
 use crate::window::Viewport;
 
-const WIDTH: u32 = 500;
+const WIDTH: u32 = 1920;
 const WIDTH_SUBPIXEL: usize = WIDTH as usize * 3;
-const HEIGHT: u32 = 500;
+const HEIGHT: u32 = 1080;
 const PIXEL_COUNT: usize = (WIDTH * HEIGHT) as usize;
 const BUFFER_SIZE: usize = PIXEL_COUNT * 3;
 
 // Change these to alter the simulation
 const TREE_SPAWN_RATE: u32 = 10;
 const LIGHTNING_SPAWN_RATE: u32 = 150;
-const SIM_SPEED: u32 = 200; // that sets the target FPS of the sim
-// but it's limited by the PC speed
+const SIM_SPEED: u32 = 200;
 
 fn main() {
     let quit = Arc::new(AtomicBool::new(false));
--- src/simulation.rs
+++ src/simulation.rs
@@ -14,57 +14,46 @@ use crate::{
     WIDTH_SUBPIXEL,
 };
 
-fn is_fire(index: usize, buffer: &[u8]) -> bool {
-    buffer[index] == 255 && buffer[index + 1] == 0 && buffer[index + 2] == 0
-}
-
-fn set_fire(index: usize, buffer: &mut [u8]) {
-    buffer[index] = 255;
-    buffer[index + 1] = 0;
-    buffer[index + 2] = 0;
-}
-
 fn is_tree(index: usize, buffer: &[u8]) -> bool {
     buffer[index] == 0 && buffer[index + 1] == 255 && buffer[index + 2] == 0
 }
 
-fn burn_trees(buffer: &mut [u8]) {
-    // tmp_buffer is needed to not change the original buffer while checking it
-    // otherwise errors occure
-    let mut tmp_buffer = vec![0; BUFFER_SIZE];
-    tmp_buffer.copy_from_slice(buffer); // copy buffer into tmp_buffer
-    for i in 0..PIXEL_COUNT {
-        if is_fire(i * 3, buffer) {
-            // check if a pixel is on fire
-            // check surrounding pixel if its a tree and...
-            let index = i * 3;
-            let positions = [
-                // top_left
-                index - WIDTH_SUBPIXEL - 3,
-                // top
-                index - WIDTH_SUBPIXEL,
-                // top_right
-                index - WIDTH_SUBPIXEL + 3,
-                // right
-                index + 3,
-                // down_right
-                index + WIDTH_SUBPIXEL + 3,
-                // down
-                index + WIDTH_SUBPIXEL,
-                // down_left
-                index + WIDTH_SUBPIXEL - 3,
-                // left
-                index - 3,
-            ];
+fn clear_pixel(buffer: &mut [u8], index: usize) {
+    buffer[index] = 0;
+    buffer[index + 1] = 0;
+    buffer[index + 2] = 0;
+}
 
-            for index in positions.iter() {
-                if is_tree(*index, buffer) {
-                    set_fire(*index, &mut tmp_buffer); // ...burn it
-                }
+fn burn_trees(buffer: &mut [u8], fire_buf: &mut Vec<usize>) {
+    let mut fire_next: Vec<usize> = Vec::new();
+    for index in fire_buf.iter() {
+        let positions = [
+            // top_left
+            index - WIDTH_SUBPIXEL - 3,
+            // top
+            index - WIDTH_SUBPIXEL,
+            // top_right
+            index - WIDTH_SUBPIXEL + 3,
+            // right
+            index + 3,
+            // down_right
+            index + WIDTH_SUBPIXEL + 3,
+            // down
+            index + WIDTH_SUBPIXEL,
+            // down_left
+            index + WIDTH_SUBPIXEL - 3,
+            // left
+            index - 3,
+        ];
+        for position in positions.iter() {
+            if is_tree(*position, buffer) {
+                clear_pixel(buffer, *position);
+                fire_next.push(*position);
             }
         }
     }
-    buffer.copy_from_slice(&tmp_buffer);
+    fire_buf.clear();
+    fire_buf.append(&mut fire_next);
 }
 
 pub struct Simulation {
@@ -116,21 +105,12 @@ impl Simulation {
         buffer[index] == 0 && buffer[index + 1] == 0 && buffer[index + 2] == 0
     }
 
-    fn clear_pixel(&self, index: usize) {
-        let mut buffer = self.buffer.lock().unwrap();
-        buffer[index] = 0;
-        buffer[index + 1] = 0;
-        buffer[index + 2] = 0;
-    }
-
-    // leave only edge fire and extinguish every other
-    fn clear_fire(&self, prev_frame: &[u8]) {
-        for n in 0..PIXEL_COUNT {
-            if is_fire(n * 3, prev_frame) {
-                self.clear_pixel(n * 3);
-            }
-        }
-    }
+    // // leave only edge fire and extinguish every other
+    // fn clear_fire(&self, prev_frame: &[u8], fire_buf: &mut Vec<usize>) {
+    //     for n in 0..fire_buf.len() {
+    //         self.clear_pixel(fire_buf[n]);
+    //     }
+    // }
 
     pub fn run(self, quit: Arc<AtomicBool>) -> JoinHandle<()> {
         thread::spawn(move || {
@@ -138,7 +118,7 @@ impl Simulation {
             let mut rng = rand::rng();
             let mut frame_count = 0;
 
-            let mut prev_buffer: Vec<u8> = vec![0; BUFFER_SIZE];
+            let mut fire_buf: Vec<usize> = Vec::new();
 
             for _ in 0..(PIXEL_COUNT as f32 * 0.5).round() as usize {
                 simulation.set_tree(rng.random_range(0..PIXEL_COUNT) * 3);
@@ -157,11 +137,12 @@ impl Simulation {
                     }
                 }
                 // every other frame spawn a lightning on rng location
-                let mut buffer = simulation.buffer.lock().unwrap();
+                let buffer = simulation.buffer.lock().unwrap();
                 if frame_count >= LIGHTNING_SPAWN_RATE {
                     let spawn_point = rng.random_range(0..PIXEL_COUNT) * 3;
                     if is_tree(spawn_point, &buffer) {
-                        set_fire(spawn_point, &mut buffer);
+                        fire_buf.push(spawn_point);
+                        // set_fire(spawn_point, &mut buffer);
                         frame_count = 0;
                     }
                 }
@@ -170,13 +151,9 @@ impl Simulation {
                 simulation.clear_edges();
                 // spread the fire
                 let mut buffer = simulation.buffer.lock().unwrap();
-                burn_trees(&mut buffer);
+                burn_trees(&mut buffer, &mut fire_buf);
                 drop(buffer);
-                // clean fire so only edge fire remains
-                simulation.clear_fire(&prev_buffer);
 
-                // copy current buffer into prev_buffer
-                prev_buffer.copy_from_slice(&simulation.buffer.lock().unwrap());
                 // increase frame_count for frame depending lightning spawn
                 frame_count += 1;
                 ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / SIM_SPEED));
